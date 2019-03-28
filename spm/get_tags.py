2c7df60c9197c9af60c8439d5596e2ce9f8d1324
Dan Bright <productions@aninstance.com>
2019-03-28 17:03:53 +0000

    Add method to write IPTC tags

--- spm/get_tags.py
+++ spm/get_tags.py
@@ -23,50 +23,60 @@ class ProcessImages:
         method to read IPTC tags
         :param filename: filename of image
         :param path: path to image
-        :return: {'path': {'filename': ['a tag 1', 'a tag 2']}} | False
+        :return: {'path': path, 'filename': filename, 'key': key, 'tags': ['tag 1', 'tag 2']} | False
         """
-        image_data = {path: dict()}
         try:
+            image_data = {}
             url = os.path.join(path, filename)
             meta = pyexiv2.ImageMetadata(os.path.join(url))
             meta.read()
             iptc_keys = meta.iptc_keys
             for key in iptc_keys:
                 tag = meta[key]
-                image_data[path][filename] = tag.raw_value
+                image_data = {'path': path, 'filename': filename, 'iptc_key': key, 'tags': tag.raw_value}
+            return image_data
         except IOError as e:
             print(f'An error occurred: {e}')
             return False
-        return image_data
 
-    def write_iptc_tags(self, orig_filename, orig_path, converted_filename, converted_path):
+    def write_iptc_tags(self, path, filename, tag_data):
         """
-         method to copy IPTC tags from between images
-        :param orig_filename: filename of image to copy from
-        :param orig_path: path of image to copy from
-        :param converted_filename: filename of image to copy to
-        :param converted_path: path of image to copy to
+        method to write new IPTC tags to image
+        :param path: path to image
+        :param filename: filename of image
+        :param tag_data: original image data: in form:
+            {'path': path, 'filename': filename, 'iptc_key': key, 'tags': ['tag 1', 'tag 2']}
         :return: True | False
         """
-        # Read tags from original image
-        orig_tags = self.read_iptc_tags(filename=orig_filename, path=orig_path)
-        print(orig_tags)
+        try:
+            iptc_key = tag_data['iptc_key']
+            tags = tag_data['tags']
+            url = os.path.join(path, filename)
+            meta = pyexiv2.ImageMetadata(os.path.join(url))
+            meta.read()
+            meta[iptc_key] = pyexiv2.IptcTag(iptc_key, tags)
+            meta.write()
+            print('Tags successfully written!')
+            return True
+        except (TypeError, Exception) as e:
+            print(e)
         return False
 
-    def convert_format(self, filename, path, save_directory):
+    def convert_format(self, filename, path, save_path):
         """
         method to convert the format of an image file
         :param filename: filename of image
         :param path: path of image
         :param save_directory: where to save the converted image
-        :return: True | False
+        :return: [new file path, new file filename] | False
         """
         try:
             url = os.path.join(path, filename)
             file, extension = os.path.splitext(filename)
             outfile = f'{file}.jpg'
-            Image.open(url).save(os.path.join(save_directory, outfile), quality=100)
-            return True
+            Image.open(url).save(os.path.join(save_path, outfile), quality=100)
+            print('Conversion done!')
+            return {'path': save_path, 'filename': outfile}
         except (IOError, Exception) as e:
             print(f'An error occurred: {e}')
         return False
@@ -74,11 +84,13 @@ class ProcessImages:
     def run(self):
         for filename in os.listdir(self.ORIGINAL_IMAGE_PATH):
             if not os.path.isdir(os.path.join(self.ORIGINAL_IMAGE_PATH, filename)):
+                # save copy of the image with converted format
                 converted = self.convert_format(filename=filename, path=self.ORIGINAL_IMAGE_PATH,
-                                                save_directory=self.PROCESSED_IMAGE_PATH)
-                self.write_iptc_tags(orig_filename=filename, orig_path=self.ORIGINAL_IMAGE_PATH,
-                                     converted_filename=filename, converted_path=self.PROCESSED_IMAGE_PATH)
-                print('Resize done!' if converted else 'Resize failed!')
+                                                save_path=self.PROCESSED_IMAGE_PATH)
+                # read tag data from original image
+                tag_data = self.read_iptc_tags(filename=filename, path=self.ORIGINAL_IMAGE_PATH)
+                # write tag data to the converted copy
+                self.write_iptc_tags(path=self.PROCESSED_IMAGE_PATH, filename=converted['filename'], tag_data=tag_data)
 
 
 p = ProcessImages(image_path=ORIGINAL_IMAGE_PATH, processed_image_path=PROCESSED_IMAGE_PATH)
