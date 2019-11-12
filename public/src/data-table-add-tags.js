e7e3d541c7ec5c249a9f8ba46ee4ac6e4a72297b
Aninstance <github@aninstance.com>
2019-11-12 15:21:54 +0000

    Allowed single quote character in tags

--- public/src/data-table-add-tags.js
+++ public/src/data-table-add-tags.js
@@ -16,7 +16,7 @@ const DataTableAddTags = props => {
   const tagSuggestionBlacklist = ["SPM: TAGS COPIED FROM ORIGINAL"];
 
   const _validateInput = value => {
-    return /^[a-zA-Z\d\-/(): ]*$/.test(value) ? value : tags;
+    return /^[a-zA-Z\d\-/():' ]*$/.test(value) ? value : tags;
   };
 
   const handleChangeTags = e => {
